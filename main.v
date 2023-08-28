5241396ee70ad535c785f23af800e06cce98fa4a
Will MacCormack <willmaccormack@gmail.com>
2023-08-28 15:24:46 -0400

    Fixed Case of clk and rst and add combined targets

--- main.v
+++ main.v
@@ -1,6 +1,6 @@
 module top(
-    input CLK,
-    input RST,
+    input clk,
+    input rst,
     output [7:0] LED,
     output [23:0] IO_LED
 );
@@ -8,8 +8,8 @@ module top(
     localparam COUNTER_WIDTH = 32 + 8;
     reg [COUNTER_WIDTH-1:0] counter;
 
-    always @(posedge CLK) begin
-        if (RST == 1'b0) begin
+    always @(posedge clk) begin
+        if (rst == 1'b0) begin
             counter <= 0;
         end else begin
             counter <= counter + 1;
